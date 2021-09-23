6a25f3b4d073e3ac66d3ddecdad0bb69dff2de52
hirobitch <89516353+hirobitch@users.noreply.github.com>
2021-09-23 14:52:41 +0900

    aaa

--- babel.config.js
+++ babel.config.js
@@ -5,11 +5,11 @@ module.exports = function(api) {
   var isProductionEnv = api.env('production')
   var isTestEnv = api.env('test')
 
-  if (!validEnv.includes(currentEnv)) {ら
+  if (!validEnv.includes(currentEnv)) {
     throw new Error(
       'Please specify a valid `NODE_ENV` or ' +
         '`BABEL_ENV` environment variables. Valid values are "development", ' +
-        '"test", and "production". Instead, received: ' 
+        '"test", and "production". Instead, received: ' +
         JSON.stringify(currentEnv) +
         '.'
     )
