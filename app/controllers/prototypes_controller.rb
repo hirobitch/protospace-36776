2cfeb60e6d8c26ab662e80b73d6fee01604f7829
hirobitch <89516353+hirobitch@users.noreply.github.com>
2021-09-23 11:50:54 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,66 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show]
+  before_action :move_to_index, only: :edit
+  before_action :authenticate_user!, only: [:new, :edit, :destoroy]
+  
+  def index 
+    @prototypes = Prototype.all.includes(:user)
+
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    @comment = Comment.create
+    if @prototype.save
+       redirect_to root_path(@prototype)
+    else
+      @prototypes = Prototype.includes(:user)
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+   # @user = user.prototype(params[:id]) 該当のプロトタイプに紐づくユーザー
+  end 
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+         redirect_to prototype_path(@prototype)
+    else
+        render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id 
+      redirect_to action: :index
+    end
+  end
+end
